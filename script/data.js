6ded2bcbd3b412f36c4c08cba14ee1766cc93fd9
Hurricane.TN <houssem.rouabeh@toucomex.com>
2024-12-27 17:26:35 +0100

    structure du projet

---
+++ script/data.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    name: "Pull Bleu",
+    tag: "Pull Bleu",
+    price: "15",
+    incart: "0",
+  },
+  {
+    name: "Survetment Nike",
+    tag: "Survetment Nike",
+    price: "105",
+    incart: "0",
+  },
+  {
+    name: "Survetment Nike Gris",
+    tag: "Survetment Nike Gris",
+    price: "95",
+    incart: "0",
+  },
+  {
+    name: "Pull Vert",
+    tag: "Pull Vert",
+    price: "35",
+    incart: "0",
+  },
+  {
+    name: "Pull Lacoste",
+    tag: "Pull Lacoste",
+    price: "65",
+    incart: "0",
+  },
+  {
+    name: "Jacket",
+    tag: "Jacket",
+    price: "85",
+    incart: "0",
+  },
+  {
+    name: "Jeans Femme",
+    tag: "Jeans Femme",
+    price: "135",
+    incart: "0",
+  },
+  {
+    name: "Pull Polo",
+    tag: "Pull Polo",
+    price: "110",
+    incart: "0",
+  },
+];
